6a07acfe7323c3c4a34488dac3530b2e34ba5b8f
Quentin Pré <qup@Quentins-MacBook-Pro.local>
2014-09-01 18:58:24 +0200

    [UPD] test with auto-reconnecting websockets

--- app/public/assets/js/application.js
+++ app/public/assets/js/application.js
@@ -1,13 +1,36 @@
+function WebSocketHandler (uri, onmessage) {
+  this.ws = null;
+  this.uri = uri;
+
+  this.init = function () {
+    this.ws = new WebSocket(this.uri);
+    this.ws.onmessage = this.onmessage;
+    this.ws.onclose = function () {
+      this.init();
+    }.bind(this);
+  }
+
+  this.send = function (str) {
+    if (!this.ws) {
+      this.init();
+    }
+
+    this.ws.send(str);
+  }
+}
+
 var scheme   = "ws://";
 var uri      = scheme + window.document.location.host + "/";
-var ws       = new WebSocket(uri);
-ws.onmessage = function(message) {
+
+var onmessage = function(message) {
   var data = JSON.parse(message.data);
   $("#chat-text").append("<div class='panel panel-default'><div class='panel-heading'>" + data.handle + "</div><div class='panel-body'>" + data.text + "</div></div>");
   $("#chat-text").stop().animate({
     scrollTop: $('#chat-text')[0].scrollHeight
   }, 800);
 };
+
+var ws = WebSocketHandler(uri. onmessage);
 
 $("#input-form").on("submit", function(event) {
   event.preventDefault();
